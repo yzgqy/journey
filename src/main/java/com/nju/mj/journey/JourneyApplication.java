a3746fe777f06157c52f0643c8a4a06ad635c46b
yzgqy <772751879@qq.com>
2019-01-07 17:43:29 +0800

    初始化

---
+++ src/main/java/com/nju/mj/journey/JourneyApplication.java
@@ -0,0 +1,14 @@
+package com.nju.mj.journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyApplication.class, args);
+	}
+
+}
+
